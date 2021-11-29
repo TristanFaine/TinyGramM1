d8b28f5096b1a7b0b1e7d4f40e2da17a44f4faa2
Antoine Jamelot <antoine.jamelot@outlook.fr>
2021-11-29 12:05:44 +0100

    Ajout d’utilisateur _seulement s’il n’existe pas_

--- src/main/java/api/Endpoint.java
+++ src/main/java/api/Endpoint.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Collections;
 import java.util.HashMap;
-
+import java.util.HashSet;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -22,7 +22,11 @@ import com.google.api.server.spi.config.ApiMethod.HttpMethod;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
-
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import com.google.cloud.storage.BlobId;
@@ -49,106 +53,119 @@ public class Endpoint {
         String userToken = req.getHeader("Authorization").substring(7);
 
         GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
-                .setAudience(Collections.singletonList("852760108989-gqn73cl4kuk3nb5a8mgf38rgace4u3lk.apps.googleusercontent.com"))
+                .setAudience(Collections
+                        .singletonList("852760108989-gqn73cl4kuk3nb5a8mgf38rgace4u3lk.apps.googleusercontent.com"))
                 .build();
 
-                GoogleIdToken idToken = verifier.verify(userToken);
-                if (idToken != null) {
-                    Payload payload = idToken.getPayload();
-                    String userId = payload.getSubject();
-
-                    Entity user = new Entity("User", userId);
-                    datastore.put(user);
-                    return Arrays.asList(userId);
-                } else {
-                    return Arrays.asList("Astaghfirullah mais ton token n'est pas valide ou tu n'accorde pas les autorisations nécessaires" + userToken);
-                }
+        GoogleIdToken idToken = verifier.verify(userToken);
+        if (idToken != null) {
+            Payload payload = idToken.getPayload();
+            String userId = payload.getSubject();
+            // Ajout d’un nouvel utilisateur s’il n’existe déjà
+            try {
+                datastore.get(KeyFactory.createKey("User", userId));
+            } catch (EntityNotFoundException e) {
+                Entity user = new Entity("User", userId);
+                user.setProperty("name", payload.get("given_name") + " " + payload.get("family_name"));
+                user.setProperty("following", new HashSet<String>());
+                user.setProperty("followers", new HashSet<String>());
+                datastore.put(user);
+            }
+            return Arrays.asList(userId);
+        } else {
+            return Arrays.asList(
+                    "Astaghfirullah mais ton token n'est pas valide ou tu n'accordes pas les autorisations nécessaires"
+                            + userToken);
+        }
     }
 
     @ApiMethod(name = "addImage", httpMethod = HttpMethod.POST, path = "addImage")
-    public Map<String, String> addImage(HttpServletRequest req, @Named("imageString") String imageString, @Named("description") String description) throws GeneralSecurityException, IOException, Exception {
+    public Map<String, String> addImage(HttpServletRequest req, @Named("imageString") String imageString,
+            @Named("description") String description) throws GeneralSecurityException, IOException, Exception {
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         String userToken = req.getHeader("Authorization").substring(7);
-   
 
-        //https://stackoverflow.com/questions/25309464/send-image-from-android-client-to-appengine-cloud-endpoint
+        // https://stackoverflow.com/questions/25309464/send-image-from-android-client-to-appengine-cloud-endpoint
 
         GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
-                .setAudience(Collections.singletonList("852760108989-gqn73cl4kuk3nb5a8mgf38rgace4u3lk.apps.googleusercontent.com"))
+                .setAudience(Collections
+                        .singletonList("852760108989-gqn73cl4kuk3nb5a8mgf38rgace4u3lk.apps.googleusercontent.com"))
                 .build();
 
-                GoogleIdToken idToken = verifier.verify(userToken);
+        GoogleIdToken idToken = verifier.verify(userToken);
 
-                if (idToken != null) {
-                    Payload payload = idToken.getPayload();
-                    String userId = payload.getSubject();
+        if (idToken != null) {
+            Payload payload = idToken.getPayload();
+            String userId = payload.getSubject();
 
-                    String[] parts = imageString.split("[,]");
-                    imageString = parts[1];
+            String[] parts = imageString.split("[,]");
+            imageString = parts[1];
 
-                    byte[] decode = Base64.getDecoder().decode(imageString);
-                    InputStream is = new ByteArrayInputStream(decode);
-                    String mimeType = null;
-                    String fileExtension = null;
-                    try {
-                        mimeType = URLConnection.guessContentTypeFromStream(is);
-                        String delimiter="[/]";
-                        String[] tokens = mimeType.split(delimiter);
-                        fileExtension = tokens[1];
-                    } catch (IOException ioException){
-                        throw new Exception("trucs: " + fileExtension,ioException); 
-                    }
+            byte[] decode = Base64.getDecoder().decode(imageString);
+            InputStream is = new ByteArrayInputStream(decode);
+            String mimeType = null;
+            String fileExtension = null;
+            try {
+                mimeType = URLConnection.guessContentTypeFromStream(is);
+                String delimiter = "[/]";
+                String[] tokens = mimeType.split(delimiter);
+                fileExtension = tokens[1];
+            } catch (IOException ioException) {
+                throw new Exception("trucs: " + fileExtension, ioException);
+            }
 
-                    //TODO: Définir ces trucs avec les env var de webapp\WEB-INF\appengine-web.xml
-                    // The ID of your GCP project
-                    String projectId = "projet-tinygram-tf ";
-                
-                    // The ID of your GCS bucket
-                    String bucketName = "projet-tinygram-tf.appspot.com";
-                
-                    // The ID of your GCS object
-                    String objectName = "random" + description + "." + fileExtension;
-         
-                    Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
-                    BlobId blobId = BlobId.of(bucketName, objectName);
-                    BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("image/" + fileExtension).build();
-                    storage.create(blobInfo, decode);
+            // TODO: Définir ces trucs avec les env var de webapp\WEB-INF\appengine-web.xml
+            // The ID of your GCP project
+            String projectId = "projet-tinygram-tf ";
 
+            // The ID of your GCS bucket
+            String bucketName = "projet-tinygram-tf.appspot.com";
 
-                    //TODO: créer une entité post avec les infos qu'on a.
-                    //PENSER À LE DEFINIR D'ABORD DANS INDEX.YAML!!!!
-                    Map<String, String> map = new HashMap<String, String>();
-                    map.put("result", imageString);
-                    return map;
-                } else {
-                    // Cette partie du code est si le token n'est pas reconnu | est expiré
-                    return Collections.singletonMap("error", "error : token unrecognized");
-                }
+            // The ID of your GCS object
+            String objectName = "random" + description + "." + fileExtension;
 
-    /* Tests :
-        -- 1 : Peut-il obtenir le token via header Authorization? V
-        -- 2 : Peut-il lire correctement le contenu? V
-        -- 3 : Peut-il utiliser les credentials Google correctement? V
-        -- 4 : Peut-il se connecter datastore/cloud storage pour utiliser bucket | V pour l'écriture | ? pour la lecture
-    */
+            Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+            BlobId blobId = BlobId.of(bucketName, objectName);
+            BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("image/" + fileExtension).build();
+            storage.create(blobInfo, decode);
 
-    /*Trucs à faire :
-        Faire modèle kinds posts (id, imageURL, followedbylist)
-        Peut-être faire un kind followedBy pour pouvoir le mettre à jour?
-        //On suppose qu'un user peut avoir accès aux posts des gens qu'il follow
-        //OU on suppose qu'un post est envoyè aux users inscrits
-        //je sais plus trop c'est quoi la meilleur façon
+            // TODO: créer une entité post avec les infos qu'on a.
+            // PENSER À LE DEFINIR D'ABORD DANS INDEX.YAML!!!!
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("result", imageString);
+            return map;
+        } else {
+            // Cette partie du code est si le token n'est pas reconnu | est expiré
+            return Collections.singletonMap("error", "error : token unrecognized");
+        }
 
-        Produire méthode : getImage | getTimeline
+        /*
+         * Tests :
+         * -- 1 : Peut-il obtenir le token via header Authorization? V
+         * -- 2 : Peut-il lire correctement le contenu? V
+         * -- 3 : Peut-il utiliser les credentials Google correctement? V
+         * -- 4 : Peut-il se connecter datastore/cloud storage pour utiliser bucket | V
+         * pour l'écriture | ? pour la lecture
+         */
 
+        /*
+         * Trucs à faire :
+         * Faire modèle kinds posts (id, imageURL, followedbylist)
+         * Peut-être faire un kind followedBy pour pouvoir le mettre à jour?
+         * //On suppose qu'un user peut avoir accès aux posts des gens qu'il follow
+         * //OU on suppose qu'un post est envoyè aux users inscrits
+         * //je sais plus trop c'est quoi la meilleur façon
+         * 
+         * Produire méthode : getImage | getTimeline
+         * 
+         * 
+         * 
+         */
 
-
-    */
-    
-    //https://cloud.google.com/appengine/docs/standard/java/using-cloud-storage
-    //https://cloud.google.com/storage/docs/reference/libraries#client-libraries-install-java
-    //https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-java
-    //https://cloud.google.com/storage/docs/downloading-objects#storage-download-object-java
+        // https://cloud.google.com/appengine/docs/standard/java/using-cloud-storage
+        // https://cloud.google.com/storage/docs/reference/libraries#client-libraries-install-java
+        // https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-java
+        // https://cloud.google.com/storage/docs/downloading-objects#storage-download-object-java
 
     }
 }
